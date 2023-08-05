a21bee5bb12e3a34cfe760efa5a694fd6e6bdf6b
Kahhow <kl1669@nyu.edu>
2023-08-05 15:03:14 +0800

    confirmed deelete and create works via postman

---
+++ app/api/[id]/route.js
@@ -0,0 +1,21 @@
+import connectMongoDB from "@/libs/mongodb";
+import Wine from "@/models/wine";
+import { NextResponse } from "next/server";
+
+export async function PUT(request, { params }){
+    const { id } = params;
+    const { newName: name, newType: type, newStatus: status, newPrice: price, newDescription: description } = await request.json();
+    await connectMongoDB();
+    await Wine.findByIdAndUpdate(id, { name, type, status, price, description });
+    if (!updatedWine) {
+        return response.status(404).json({ message: "Wine not found" });
+    }
+    return NextResponse.json({ message: "Wine Updated"}, { status:200});
+}
+
+export async function GET(request,{ params }){
+    const { id } = params;
+    await connectMongoDB();
+    const wine = await Wine.findOne({_id: id});
+    return NextResponse.json({ topic }, {status: 200})
+}
--- app/api/wines/route.js
+++ app/api/wines/route.js
@@ -2,7 +2,8 @@ import connectMongoDB from "@/libs/mongodb";
 import Wine from "@/models/wine";
 import { NextResponse } from "next/server";
 
-// create routes
+// create routes for POST
+// confirmed POST works via postman
 export async function POST(request){
     const { name, type, status, price, description } = await request.json();
     await connectMongoDB();
@@ -10,3 +11,39 @@ export async function POST(request){
     return NextResponse.json({ message:"Wine Created" }, { status: 201 });
 }
 
+
+// create routes for GET
+// confirmed GET works via postman
+export async function GET(request){
+    await connectMongoDB();
+    const wines = await Wine.find();
+    return NextResponse.json({ wines });
+}
+
+// create routes for DELETE
+// confirmed DELETE works via postman
+export async function DELETE(request){
+    const id = request.nextUrl.searchParams.get("id");
+    await connectMongoDB();
+        // Find the wine first before deletion
+        const wine = await Wine.findById(id);
+        if (!wine) {
+            // Wine not found, return a 404 status
+            return NextResponse.json({ message:"Wine not found" }, { status: 404 });
+        }
+    
+        // Store wine details
+        const wineDetails = {
+            name: wine.name,
+            type: wine.type,
+            status: wine.status,
+            price: wine.price,
+            description: wine.description
+        };
+    
+        // Delete the wine
+        await Wine.findByIdAndDelete(id);
+    
+        // Return the details of the deleted wine
+        return NextResponse.json({ message: `Deleted wine with ID: ${id} and details: ${JSON.stringify(wineDetails)}.` }, { status: 200 });    
+}
