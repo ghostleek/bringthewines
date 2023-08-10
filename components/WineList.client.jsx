d098c84c1b9986185a75d33a34b5bcee81915cbd
Kahhow <kl1669@nyu.edu>
2023-08-10 17:36:54 +0800

    shifted button to right and aligned to ctscore

--- components/WineList.client.jsx
+++ components/WineList.client.jsx
@@ -58,16 +58,52 @@ export default function WineList() {
         <>
         <div className="inline-flex justify-between w-full">
             <div>
-                {/* Existing filter buttons */}
-                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-l" onClick={() => setSelectedType("All")}>All</button>
-                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4" onClick={() => setSelectedType("Red")}>Red</button>
-                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-r" onClick={() => setSelectedType("White")}>White</button>
+                {/* Type filter buttons */}
+                <button 
+                className={`bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-l ${selectedType === "All" ? 'bg-gray-400 text-white' : ''}`} 
+                onClick={() => setSelectedType("All")}
+                onDoubleClick={() => setSelectedType("All")}
+            >
+                All
+            </button>
+            <button 
+                className={`bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 ${selectedType === "Red" ? 'bg-gray-400 text-white' : ''}`} 
+                onClick={() => setSelectedType("Red")}
+                onDoubleClick={() => setSelectedType("All")}
+            >
+                Red
+            </button>
+            <button 
+                className={`bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-r ${selectedType === "White" ? 'bg-gray-400 text-white' : ''}`} 
+                onClick={() => setSelectedType("White")}
+                onDoubleClick={() => setSelectedType("All")}
+            >
+                White
+            </button>
             </div>
             <div>
-                {/* New sort by price buttons */}
-                <button className="bg-gray-100 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-l" onClick={() => setPriceFilter("$")}>$</button>
-                <button className="bg-gray-100 hover:bg-gray-400 text-gray-800 py-2 px-4" onClick={() => setPriceFilter("$$")}>$$</button>
-                <button className="bg-gray-100 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-r" onClick={() => setPriceFilter("$$$")}>$$$</button>
+                {/* Price filter buttons */}
+                <button 
+                    className={`bg-gray-100 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-l ${priceFilter === "$" ? 'bg-gray-400 text-white' : ''}`} 
+                    onClick={() => setPriceFilter(priceFilter === "$" ? null : "$")}
+                    onDoubleClick={() => setPriceFilter(null)}
+                >
+                    $
+                </button>
+                <button 
+                    className={`bg-gray-100 hover:bg-gray-400 text-gray-800 py-2 px-4 ${priceFilter === "$$" ? 'bg-gray-400 text-white' : ''}`} 
+                    onClick={() => setPriceFilter(priceFilter === "$$" ? null : "$$")}
+                    onDoubleClick={() => setPriceFilter(null)}
+                >
+                    $$
+                </button>
+                <button 
+                    className={`bg-gray-100 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-r ${priceFilter === "$$$" ? 'bg-gray-400 text-white' : ''}`} 
+                    onClick={() => setPriceFilter(priceFilter === "$$$" ? null : "$$$")}
+                    onDoubleClick={() => setPriceFilter(null)}
+                >
+                    $$$
+                </button>
             </div>
         </div>
         {filteredWines.map((t) => {
@@ -79,34 +115,38 @@ export default function WineList() {
             const whatsappURL = `https://wa.me/+6597383295?text=${encodedMessage}`;
 
             return (
-                <div 
-                key={t._id}
-                className="mt-4 p-4 border border-slate-300 rounded-md flex flex-col gap-5">
+                <><div
+                    key={t._id}
+                    className="mt-4 p-4 border border-slate-300 rounded-md flex flex-col gap-5">
                     <div>
                         <div className="flex justify-between">
                             <div className="pl-1" style={{ color: t.type === 'Red' ? 'red' : t.type === 'White' ? 'grey' : 'inherit' }}>
                                 {t.type}
                             </div>
                             <div className="flex gap-2">
-                                <RemoveBtn id={t._id}/>
+                                <RemoveBtn id={t._id} />
                                 <Link href={`/editWine/${t._id}`}>
                                     <HiPencilAlt size={24} />
                                 </Link>
                             </div>
                         </div>
-                    <div class = "flex pb-1"><h2 className="font-light bg-stone-100 rounded-md p-2"> {t.vintage} </h2><h2 className = "font-light text-xl p-2">{t.name}</h2></div>
-                    <div className = "pb-2">{t.description}</div>
-                        <div class = "flex"><h3 className = "font-bold text-xl">${t.price} </h3></div>
+                        <div class="flex pb-1"><h2 className="font-light bg-stone-100 rounded-md p-2"> {t.vintage} </h2><h2 className="font-light text-xl p-2">{t.name}</h2></div>
+                        <div className="pb-2">{t.description}</div>
+                        <div className="mt-1">
+                            <h3 className = "font-bold text-xl">${t.price} </h3>
+                        </div>
+                    </div>
+                    <div className="flex justify-between items-center">
                         <div className="flex items-center gap-2">
-                                <span>{t.ctscore}</span>
-                                <div className="relative inline-block hover:cursor-pointer group">
+                            <span>{t.ctscore}</span>
+                            <div className="relative inline-block hover:cursor-pointer group">
                                 <HiOutlineInformationCircle className="hover:opacity-100" />
                                 <div className="absolute left-full top-1/2 transform -translate-y-1/3 w-72 p-2 text-xs bg-white border rounded-md shadow-lg opacity-0 group-hover:opacity-100 z-10">
                                     Score based on Parker scoring system. <br></br>
-                                    
-                                    <a class = "red"
-                                        href="https://www.wineinvestment.com/sg/learn/magazine/2021/05/the-role-of-wine-critics-2/" 
-                                        target="_blank" 
+
+                                    <a class="red"
+                                        href="https://www.wineinvestment.com/sg/learn/magazine/2021/05/the-role-of-wine-critics-2/"
+                                        target="_blank"
                                         rel="noopener noreferrer"
                                     >
                                         Read more
@@ -114,23 +154,26 @@ export default function WineList() {
                                 </div>
                             </div>
                         </div>
+
                         {/* Show the WhatsApp button if the status is "Available" */}
-                        <div class = "pt-3 flex justify-end">
-                        {t.status === 'Available' ? (
-                            <a href={whatsappURL} target="_blank" rel="noopener noreferrer">
-                            <button className="bg-black text-white p-2 rounded-md w-28">
-                                Buy now
-                            </button>
-                            </a>
-                        ) : t.status === 'Reserved' ? (
-                            <button className="bg-gray-300 text-gray-600 p-2 rounded-md w-28" disabled>
-                            Reserved
-                            </button>
-                        ) : null}                    
-                    </div>                
+                        <div>
+                            {t.status === 'Available' ? (
+                                <a href={whatsappURL} target="_blank" rel="noopener noreferrer">
+                                    <button className="bg-black text-white p-2 rounded-md w-28">
+                                        Buy now
+                                    </button>
+                                </a>
+                            ) : t.status === 'Reserved' ? (
+                                <button className="bg-gray-300 text-gray-600 p-2 rounded-md w-28" disabled>
+                                    Reserved
+                                </button>
+                            ) : null}
+                        </div>
                     </div>
                 </div>
-                );
+                
+                    </>
+        );        
         })}
         </>
     );
