63e04fad8b55b401ef62c7ba1baae54ef70172f9
Kahhow <kl1669@nyu.edu>
2023-08-10 16:52:25 +0800

    created functional filter for types of wine

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
        {/* centre the div and set max width */}
       <div className="max-w-3xl mx-auto p-4">
       <Navbar />
-      <div className="mt-8">{children}</div>
+      <div className="mt-4">{children}</div>
       </div>
       </body>
     </html>
