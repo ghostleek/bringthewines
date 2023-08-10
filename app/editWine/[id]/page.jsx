c8ba780b4a6e34e68d03c77e408dbc6bcfed9479
Kahhow <kl1669@nyu.edu>
2023-08-10 14:46:32 +0800

    refactored edit wine page

--- app/editWine/[id]/page.jsx
+++ app/editWine/[id]/page.jsx
@@ -3,28 +3,54 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const getWineById = async(id) => {
-    try{
-        const res = await fetch(`${process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://bringthewines.vercel.app'}/api/wines/${id}`, {
-            cache: "no-store", 
-        });
-        if (!res.ok) {
-            throw new Error("Failed to fetch Wine");
+const BASE_URL = process.env.NODE_ENV === 'development' 
+    ? 'http://localhost:3000' 
+    : 'https://bringthewines.vercel.app';
+
+    const getWineById = async(id) => {
+        const endpoint = `${BASE_URL}/api/wines/${id}`;
+    
+        try {
+            const res = await fetch(endpoint, { cache: "no-store" });
+            
+            if (!res.ok) {
+                const errorData = await res.json(); // Might provide additional error details
+                throw new Error(`Failed to fetch Wine: ${errorData.message || res.status}`);
+            }
+    
+            return await res.json();
+        } catch (error) {
+            // write clearer error messages
+            console.error('Error fetching wine:', error.message);
+            throw error; 
         }
-        console.log("Response:", res); // Log the response
-        return await res.json();
-    } catch (error) {
-        console.log(error);
+    };
+    
+    export default async function EditWine({ params }) {
+        const { id } = params;
+        
+        try {
+            const wineData = await getWineById(id);
+            console.log('Wine:', wineData);
+            console.log("id: ", id);
+    
+            const { name, price, description, status, type, ctscore, vintage } = wineData;
+    
+            return (
+                <EditWineForm 
+                    id={id} 
+                    name={name} 
+                    price={price} 
+                    description={description} 
+                    status={status} 
+                    type={type} 
+                    vintage={vintage} 
+                    ctscore={ctscore} 
+                />
+            );
+        } catch (error) {
+            // Display error on front-end + backend console log
+            console.error('Error in EditWine function:', error.message);
+            return <div>Error loading wine data.</div>;
+        }
     }
-};
-
-export default async function EditWine({ params }){
-    const { id } = params;
-    const { wine } = await getWineById(id);
-    console.log('Wine:', wine); // check wine data strcutrue
-    console.log("id: ", id)
-    const { name, price, description, status, type, ctscore, vintage } = wine;
-
-    return <EditWineForm id={id} name={name} price={price} description={description} status={status} type={type} vintage={vintage} ctscore={ctscore}/>
-}
-
