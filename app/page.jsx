096442d3fee371632506c779369b251c8549d87b
Kahhow <kl1669@nyu.edu>
2023-08-15 17:36:20 +0800

    successfully added GA to site

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import connectMongoDB from "@/libs/mongodb";
 import dynamic from 'next/dynamic';
+import Script from 'next/script'
+// import connectMongoDB from "@/libs/mongodb";
 
 // Dynamically import the WineList for client-side rendering
 const WineList = dynamic(() => import('@/components/WineList.client'), {
@@ -10,26 +10,25 @@ const WineList = dynamic(() => import('@/components/WineList.client'), {
 export default function Home() {
   return (
     <>
-      <Head>
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=YOUR-GA-MEASUREMENT-ID`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'YOUR-GA-MEASUREMENT-ID');
-            `,
-          }}
-        />
-      </Head>
+    <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
+    <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+    />
       <WineList />
     </>
   );
 }
 
 // Confirm that MongoDB is connected
-connectMongoDB();
+// connectMongoDB();
