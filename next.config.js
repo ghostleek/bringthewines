66190f1c423ba6037b736f79ff7d32e5996c20ac
Kahhow <kl1669@nyu.edu>
2023-08-15 20:17:42 +0800

    updated cors to accommodate multiple origins properly

--- next.config.js
+++ next.config.js
@@ -1,19 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
 module.exports = {
     async headers() {
       return [
         {
           source: "/api/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "http://localhost:3000, https://www.bringthewines.sg" },
-            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ]
+          headers: (req) => {
+            const allowedOrigins = ['http://localhost:3000', 'https://www.bringthewines.sg'];
+            const origin = req.headers.origin || '';
+            if (allowedOrigins.includes(origin)) {
+              return [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: origin },
+                { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+              ];
+            }
+            return []; // Default response if origin is not in the allowed list
+          }
         }
       ]
     }
   };
-  
